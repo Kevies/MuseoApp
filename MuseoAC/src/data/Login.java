5b5191f191f01bc81dc00644274cdb21a1b2295a
BuildTools <unconfigured@null.spigotmc.org>
2020-05-14 23:37:23 +0200

    Version 1.3 con menus estructurados

--- MuseoAC/src/data/Login.java
+++ MuseoAC/src/data/Login.java
@@ -12,47 +12,61 @@ import java.util.Scanner;
  * @author Kevin Martinez y Diego Fernandez
  * La clase de Login que se encargara de poder iniciar sesion segun el tipo de usuario
  */
-public class Login extends Menu{
+public class Login extends Menu
+{
     int login;
     String leerUsuarioC, leerContraseñaC;
-    public Login() {
-    }
+    public Login() {}
     /**
      * Este método hace que se vea el menú de login y podemos iniciar sesión como usuario o administrador
      * Depende de como hemos iniciado sesión llamaremos al metodo "MenuUsuario/MenuAdministrador"
      * Tambien hemos creado un objeto "Menu" llamado "menu1" para poder llamar a los métodos correspondientes a esa clase.
      */
-    public void VisualizarLogin(){
+    public void VisualizarLogin()
+    {
         Scanner sc = new Scanner(System.in);
         Menu menu1 = new Menu();
         System.out.println("Bienvenido al Museo AC");
-        while (login != 3) {
+        while (login != 3)
+        {
             System.out.println("¿Quieres iniciar sesion como usuario o administrador?\n1. Usuario\n" +
                     "2. Administrador\n3. Salir");
+            
+            System.out.print("Escribe un numero: ");
+            
             login = sc.nextInt();
             sc.nextLine();
-            switch(login){
+            switch(login)
+            {
                 case 1:
+                    
+                    int tipo=0;//variable que hara decicidir a los menos si eligen menu de admin o user
                     System.out.println("==========================");
-                    System.out.println("Escribe tu nombre de usuario:");
+                    System.out.print("Escribe tu nombre de usuario: ");
                     leerUsuarioC = sc.next();
-                    System.out.println("Escribe tu contraseña:");
+                    System.out.print("Escribe tu contraseña: ");
                     leerContraseñaC = sc.next();
-                    if(leerContraseñaC.equalsIgnoreCase("usuario1234")){
-                        menu1.MenuUsuario();
+                    if(leerContraseñaC.equalsIgnoreCase("usuario1234"))
+                    {
+                        menu1.menuZona(tipo);
                     }
+                    break;
                 case 2:
+                    
+                    int tipo2=1;
                     System.out.println("==========================");
                     System.out.println("Escribe tu nombre de usuario:");
                     leerUsuarioC = sc.next();
                     System.out.println("Escribe tu contraseña:");
                     leerContraseñaC = sc.next();
-                    if(leerContraseñaC.equalsIgnoreCase("admin1234")){
-                        menu1.MenuAdmin();
+                    if(leerContraseñaC.equalsIgnoreCase("admin1234"))
+                    {
+                        menu1.menuZona(tipo2);
                     }
+                    break;
                         
             }
-        }
+        break;}
     }
 }
     
--- MuseoAC/src/data/Menu.java
+++ MuseoAC/src/data/Menu.java
@@ -11,11 +11,16 @@ import java.util.Scanner;
  *
  * @author Kevin Martinez y Diego Fernandez
  * El menu contiene todos los menus de los distintos tipos de usuario.
+ * y las variables que tienen en comun los menus como las opciones, separadores, mesajes finales...
  */
 public class Menu extends Main{
     Zonapeces ejemplo = new Zonapeces();
     int opcionA;
     Scanner sc = new Scanner(System.in);
+    String opcionesUsuario="1. Donar elemento\n2. Ver todos los elementos\n3. Buscar elementos\n4. Ver elementos donados\n5. Ver elementos restantes\n6. Salir";
+    String opcionesAdmin="1. Añadir elemento\n2. Modificar elemento\n3. Eliminar elemento \n4. Salir";
+    String separador="==========================";
+    String numfinal="introduce un numero: ";
     /**
      * El constructor del menu.
      */
@@ -24,25 +29,27 @@ public class Menu extends Main{
     }
     /**
      * El menu del administrador, aqui albergara los distintos métodos en futuras actualizaciones.
+     * (Puede que esto se borre en actualizaciones)
      */
     public void MenuAdmin(){
         
         while (opcionA != 4) {
-                        System.out.println("1. Añadir elemento\n2. Modificar elemento\n" +
-                                "3. Eliminar elemento \n4. Salir");
+                        System.out.println(opcionesAdmin);
+                        System.out.print(numfinal);
                         opcionA = sc.nextInt();
-                        sc.nextLine(); 
+                        sc.nextLine();
+                        
                         switch (opcionA) {
                             case 1:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.añadir();
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 break;
                             case 2:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.modificar();
                             case 3:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.eliminar();
                                 break;                           
                         }
@@ -51,39 +58,152 @@ public class Menu extends Main{
     }
     /**
      * El menu del usuario, aqui albergara los distintos métodos en futuras actualizaciones.
+     * (Puede que esto se borre en actualizaciones)
      */
     public void MenuUsuario(){
+       
         while (opcionA != 6) {
-                        System.out.println("1. Donar elemento\n2. Ver todos los elementos\n" +
-                                "3. Buscar elementos \n4. Ver elementos donados\n5. Ver elementos restantes\n6. Salir");
+                        System.out.println(opcionesUsuario);
+                        System.out.print(numfinal);
                         opcionA = sc.nextInt();
                         sc.nextLine(); 
                         switch (opcionA) {
                             case 1:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.donar();
                                 System.out.println("Has donado");
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 break;
                             case 2:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 ejemplo.verElementos();
                             case 3:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.buscar();
                                 System.out.println("Estas buscando un fósil");
                                 break;
                             case 4:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.verDonados();
                                 System.out.println("Estas viendo tus fósiles donados");
                             case 5:
-                                System.out.println("==========================");
+                                System.out.println(separador);
                                 //ac.verRestantes();
                                 System.out.println("Estas viendo los fósiles que te faltan");
                         }
                     }
         System.out.println("Gracias por todo");
     }
+    /**
+     * Este metodo muestra un menu con las zonas que tiene el museo
+     * 
+     * @param tipo segun el usuario que inicie sesion un
+     * menu u otro, este parametro hace que el metodo sepa
+     * cual mostrar
+     */
+    public void menuZona(int tipo)
+    {
+        System.out.println(separador);
+        System.out.println("¿A que zona quieres ir?: ");
+        System.out.println("1.Fosiles \n2.Peces \n3.Insectos \n4.Obras");
+        System.out.print(numfinal);
+        opcionA=sc.nextInt();
+        
+        switch(opcionA)
+        {
+            case 1:
+                
+            case 2:
+                this.menuPeces(tipo);
+                break;
+            case 3:
+                this.menuInsectos();
+                break;
+            case 4:
+                this.menuObras();
+                break;
+        }
+    }
+    /**
+     * Muestra el menu de la zona de peces, con las opciones para
+     * usuario o admin, respectivamente 
+     * 
+     * @param tipo segun el usuario que inicie sesion un
+     * menu u otro, este parametro hace que el metodo sepa
+     * cual mostrar
+     */
+    public void menuPeces(int tipo)
+    {
+    switch(tipo)
+        {
+        case 0:
+            System.out.println(separador);
+            System.out.println("!!Bienvenido a la zona de Peces¡¡");
+            System.out.println(opcionesUsuario);
+            break;
+        
+        case 1:
+            System.out.println(separador);
+            System.out.println(tipo);
+            System.out.println("!!Bienvenido a la zona de Peces¡¡");
+            System.out.println(opcionesAdmin);
+            System.out.print(numfinal);
+            break;
+        }
     
+    
+    }
+    /**
+     * Muestra el menu de la zona de peces, con las opciones para
+     * usuario o admin, respectivamente 
+     * 
+     * (falta añadir el int)
+     */
+    public void menuInsectos()
+    {
+    System.out.println(separador);
+    System.out.println("!!Bienvenido a la zona de Insectos¡¡");
+    System.out.println(opcionesUsuario);
+    System.out.print(numfinal);
+    }
+    /**
+     * Muestra el menu de la zona de peces, con las opciones para
+     * usuario o admin, respectivamente 
+     * 
+     * (falta añadir el int)
+     * (falta un if dentro de los case para que elija a admin o user)
+     */
+    public void menuObras()
+    {
+    System.out.println(separador);
+    System.out.println("!!Bienvenido a la zona de Artes¡¡");
+    System.out.println("1.Ir a pinturas \n2.Ir a esculturas");
+    System.out.print(numfinal);
+    opcionA=sc.nextInt();
+        switch(opcionA)
+        {
+            case 1:
+                System.out.println(separador);
+                System.out.println(opcionesUsuario);
+                
+            case 2:
+                System.out.println(separador);
+                System.out.println(opcionesUsuario);
+        }
+    }
+    /**
+     *  Muestra el menu de la zona de peces, con las opciones para
+     * usuario o admin, respectivamente 
+     * 
+     * (falta añadir el int)
+     * (falta la estructura de los fosiles y las partes)
+     */
+    public void menuFosiles()
+    {
+        System.out.println(separador);
+        System.out.println("opciones");
+        System.out.println(numfinal);
+        opcionA=sc.nextInt();
+        switch(opcionA){}
+    }
 }
